37e61b1751cd588c3f566503738e3138557016bd
Palit Wiboonlit <s7070085@kmitl.ac.th>
2014-11-30 18:19:00 +0700

    Prototype version 1

--- pricepost.py
+++ pricepost.py
@@ -1,5 +1,7 @@
-"""Testing only"""
-def pricepost():
-    """Testing commit only"""
-    print "Test"
-pricepost()
+"""A first window and GUI"""
+from Tkinter import *
+root = Tk()
+root.title("PricePost (Prototype)")
+top_label = Label(root, text="Welcome to PricePost")
+top_label.pack()
+root.mainloop()
