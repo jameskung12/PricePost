9178dfc17037f1970ffe4f5a975235b425cdbfb3
Palit Wiboonlit <s7070085@kmitl.ac.th>
2014-12-06 15:58:07 +0700

    With parcel functions, but w/o debug on float

--- pricepost.py
+++ pricepost.py
@@ -1,12 +1,23 @@
-"""Added on click function on Exit button"""
+"""Parcel function available, w/o debug for floats"""
 from Tkinter import *
 def w_exit():
     import sys; sys.exit()
+def parcel(fees=20):
+    """
+    input : weight of your parcel (in kgs)
+    output : fees of parcel sending
+    limitation is 20kgs
+    """
+    print "Please enter weight of parcel"
+    weight = input()
+    for i in range(1, weight):
+        fees += 15
+    print "Total fees is:", fees, "BHT"
 root = Tk()
 root.title("PricePost (Prototype)")
 top_label = Label(root, text="Welcome to PricePost")
 btn_post = Button(root, text="Click here for postal")
-btn_parcel = Button(root, text="Click here for parcel")
+btn_parcel = Button(root, text="Click here for parcel", command=parcel)
 btn_trans = Button(root, text="Click here for money transfer")
 btn_exit = Button(root, text="Exit", command=w_exit)
 top_label.pack()
