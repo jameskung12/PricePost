1a97539812a74a9b7c78bec230be092078147155
Palit Wiboonlit <s7070085@kmitl.ac.th>
2014-12-06 15:30:41 +0700

    Added button, but no on click function

--- pricepost.py
+++ pricepost.py
@@ -1,7 +1,13 @@
-"""A first window and GUI"""
+"""Added button, but still no on click function"""
 from Tkinter import *
 root = Tk()
 root.title("PricePost (Prototype)")
 top_label = Label(root, text="Welcome to PricePost")
 top_label.pack()
+btn_post = Button(root, text="Click here for postal")
+btn_parcel = Button(root, text="Click here for parcel")
+btn_trans = Button(root, text="Click here for money transfer")
+btn_post.pack()
+btn_parcel.pack()
+btn_trans.pack()
 root.mainloop()
