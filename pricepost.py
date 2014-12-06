1d41878cc45c33794f410a4a1c7f4b4ee9431a15
Palit Wiboonlit <s7070085@kmitl.ac.th>
2014-12-06 15:45:59 +0700

    Added on click function on Exit button

--- pricepost.py
+++ pricepost.py
@@ -1,13 +1,17 @@
-"""Added button, but still no on click function"""
+"""Added on click function on Exit button"""
 from Tkinter import *
+def w_exit():
+    import sys; sys.exit()
 root = Tk()
 root.title("PricePost (Prototype)")
 top_label = Label(root, text="Welcome to PricePost")
-top_label.pack()
 btn_post = Button(root, text="Click here for postal")
 btn_parcel = Button(root, text="Click here for parcel")
 btn_trans = Button(root, text="Click here for money transfer")
+btn_exit = Button(root, text="Exit", command=w_exit)
+top_label.pack()
 btn_post.pack()
 btn_parcel.pack()
 btn_trans.pack()
+btn_exit.pack()
 root.mainloop()
