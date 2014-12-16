c92f077710ecb12a0b156b7eb01d6fd6cd298f28
Palit Wiboonlit <s7070085@kmitl.ac.th>
2014-12-16 11:22:22 +0700

    Version 0.01 alpha

--- pricepost.py
+++ pricepost.py
@@ -1,144 +1,151 @@
 """
-Bug fix on parcel window result display
+PricePost, IT KMITL project Version 0.01 alpha
 """
 from Tkinter import *
 def w_exit():
-    """ window exit function """
+    """Program exit function"""
     import sys; sys.exit()
 class PricePost(object):
-    def __init__(main_win):
-        """ main program """
-        main_win.root = Tk()
-        main_win.root.title("PricePost")
-        main_win.mw_lbl1 = Label(main_win.root, text="Welcome to PricePost").pack()
-        main_win.btn_post = Button(main_win.root, text="Click here for postal and EMS", command=main_win.postal_win).pack()
-        main_win.btn_parcel = Button(main_win.root, text="Click here for normal parcel", command=main_win.parcel_win).pack()
-        main_win.btn_trans = Button(main_win.root, text="Click here for money transfer",command=main_win.money_win).pack()
-        main_win.btn_about = Button(main_win.root, text="About", command=main_win.about_win).pack()
-        main_win.btn_exit = Button(main_win.root, text="Exit", command=w_exit).pack()
-        main_win.root.mainloop()
-    def postal_win(main_win):
-        """ postal window """
-        main_win.postal = Tk()
-        main_win.postal.title("Postal sending calculation")
-        main_win.pow_label1 = Label(main_win.postal, text="Please enter weight of postal (or parcel in case of EMS) (in g)").pack()
-        main_win.po_weight = IntVar()
-        main_win.po_weight = Entry(main_win.postal, textvariable=main_win.po_weight)
-        main_win.po_weight.pack()
-        main_win.pow_label2 = Label(main_win.postal, text="Please select type").pack()
-        main_win.po_type = IntVar()
-        main_win.pow_redio1 = Radiobutton(main_win.postal, text="Normal" ,variable=main_win.po_type, value=1)
-        main_win.pow_redio2 = Radiobutton(main_win.postal, text="Registered", variable=main_win.po_type, value=2)
-        main_win.pow_redio3 = Radiobutton(main_win.postal, text="EMS", variable=main_win.po_type, value=3)
-        main_win.pow_redio1.pack()
-        main_win.pow_redio2.pack()
-        main_win.pow_redio3.pack()
-        main_win.pow_e_button = Button(main_win.postal, text="Enter", command=main_win.c_postal).pack()
-        main_win.pow_r_button = Button(main_win.postal, text="Reset", command=main_win.postal_res).pack()
-        main_win.postal.mainloop()
-    def c_postal(main_win):
-        po_calc = Postal(main_win.po_weight.get(), main_win.po_type.get())
-        res_call = po_calc.parcel()
-        main_win.paw_label2 = Label(main_win.postal, text=res_call).pack()
-    def postal_res(main_win):
-        main_win.postal.destroy()
-        main_win.postal_win()
-    def parcel_win(main_win):
-        """ parcel window """
-        main_win.parcel = Tk()
-        main_win.parcel.title("Parcel calculation")
-        main_win.paw_label1 = Label(main_win.parcel, text="Please enter weight (in kg)").pack()
-        main_win.pa_weight = IntVar()
-        main_win.pa_weight = Entry(main_win.parcel, textvariable=main_win.pa_weight)
-        main_win.pa_weight.pack()
-        main_win.paw_e_button = Button(main_win.parcel, text="Enter", command=main_win.c_parcel).pack()
-        main_win.paw_r_button = Button(main_win.parcel, text="Reset", command=main_win.parcel_res).pack()
-        main_win.parcel.mainloop()
-    def c_parcel(main_win):
-        pa_calc = Parcel(main_win.pa_weight.get())
+    def __init__(price_post):
+        """Main window"""
+        price_post.root = Tk()
+        price_post.root.title("PricePost, IT KMITL project (Version 0.01 alpha)")
+        price_post.mw_lbl1 = Label(price_post.root, text="Welcome to PricePost").pack()
+        price_post.btn_post = Button(price_post.root, text="Click here for postal and EMS", command=price_post.postal_win).pack()
+        price_post.btn_parcel = Button(price_post.root, text="Click here for normal parcel", command=price_post.parcel_win).pack()
+        price_post.btn_trans = Button(price_post.root, text="Click here for money transfer",command=price_post.money_win).pack()
+        price_post.btn_about = Button(price_post.root, text="About", command=price_post.about_win).pack()
+        price_post.btn_exit = Button(price_post.root, text="Exit", command=w_exit).pack()
+        price_post.root.mainloop()
+    def postal_win(price_post):
+        """Postal window"""
+        price_post.postal = Tk()
+        price_post.postal.title("Postal sending calculation")
+        price_post.pow_label1 = Label(price_post.postal, text="Please enter weight of postal (or parcel in case of EMS) (in g)").pack()
+        price_post.po_weight = IntVar()
+        price_post.po_weight = Entry(price_post.postal, textvariable=price_post.po_weight)
+        price_post.po_weight.pack()
+        #price_post.pow_label2 = Label(price_post.postal, text="Please select type").pack()
+        price_post.po_type = IntVar()
+        #price_post.pow_radio1 = Radiobutton(price_post.postal, text="Normal" ,variable=price_post.po_type, value=1).pack()
+        #price_post.pow_radio2 = Radiobutton(price_post.postal, text="Registered", variable=price_post.po_type, value=2).pack()
+        #price_post.pow_radio3 = Radiobutton(price_post.postal, text="EMS", variable=price_post.po_type, value=3).pack()
+        price_post.pow_e_button = Button(price_post.postal, text="Enter", command=price_post.c_postal).pack()
+        price_post.pow_r_button = Button(price_post.postal, text="Reset", command=price_post.postal_res).pack()
+        price_post.postal.mainloop()
+    def c_postal(price_post):
+        po_calc = Postal(price_post.po_weight.get(), price_post.po_type.get())
+        res_call = po_calc.postal()
+        price_post.paw_label2 = Label(price_post.postal, text=res_call).pack()
+    def postal_res(price_post):
+        price_post.postal.destroy()
+        price_post.postal_win()
+    def parcel_win(price_post):
+        """Parcel window"""
+        price_post.parcel = Tk()
+        price_post.parcel.title("Parcel calculation")
+        price_post.paw_label1 = Label(price_post.parcel, text="Please enter weight (in kg)").pack()
+        price_post.pa_weight = IntVar()
+        price_post.pa_weight = Entry(price_post.parcel, textvariable=price_post.pa_weight)
+        price_post.pa_weight.pack()
+        price_post.paw_e_button = Button(price_post.parcel, text="Enter", command=price_post.c_parcel).pack()
+        price_post.paw_r_button = Button(price_post.parcel, text="Reset", command=price_post.parcel_res).pack()
+        price_post.parcel.mainloop()
+    def c_parcel(price_post):
+        """Call parcel class and return value"""
+        pa_calc = Parcel(price_post.pa_weight.get())
         res_call = pa_calc.parcel()
-        main_win.paw_label2 = Label(main_win.parcel, text=res_call).pack()
-    def parcel_res(main_win):
-        main_win.parcel.destroy()
-        main_win.parcel_win()
-    def money_win(main_win):
-        """ money window """
-        main_win.money = Tk()
-        main_win.money.title("Money sending calculation")
-        main_win.mw_label1 = Label(main_win.money, text="Please enter total of money (in BHT)").pack()
-        main_win.mo_total = IntVar()
-        main_win.mo_type = IntVar()
-        main_win.mo_total = Entry(main_win.money, textvariable=main_win.mo_total)
-        main_win.mo_total.pack()
-        main_win.mw_label2 = Label(main_win.money, text="Please select type").pack()
-        main_win.redio1 = Radiobutton(main_win.money, text="Normal" ,variable=main_win.mo_type, value=1)
-        main_win.redio2 = Radiobutton(main_win.money, text="EMS", variable=main_win.mo_type, value=2)
-        main_win.redio3 = Radiobutton(main_win.money, text="Online", variable=main_win.mo_type, value=3)
-        main_win.redio1.pack(); main_win.redio2.pack(); main_win.redio3.pack()
-        main_win.mw_e_button = Button(main_win.money, text="Enter", command=main_win.c_money).pack()
-        main_win.mw_r_button = Button(main_win.money, text="Reset", command=main_win.money_res).pack()
-        main_win.money.mainloop()
-    def c_money(main_win):
-        mo_calc = Money(main_win.mo_total.get(), main_win.mo_type.get())
+        price_post.paw_label2 = Label(price_post.parcel, text=res_call).pack()
+    def parcel_res(price_post):
+        """Reset parcel window"""
+        price_post.parcel.destroy()
+        price_post.parcel_win()
+    def money_win(price_post):
+        """Money window"""
+        price_post.money = Tk()
+        price_post.money.title("Money sending calculation")
+        price_post.mw_label1 = Label(price_post.money, text="Please enter total of money (in BHT)").pack()
+        price_post.mo_total = IntVar()
+        price_post.mo_total = Entry(price_post.money, textvariable=price_post.mo_total)
+        price_post.mo_total.pack()
+        #price_post.mw_label2 = Label(price_post.money, text="Please select type").pack()
+        price_post.mo_type = IntVar()
+        #price_post.mo_radio1 = Radiobutton(price_post.money, text="Normal", variable=price_post.mo_type, value=1).pack()
+        #price_post.mo_radio2 = Radiobutton(price_post.money, text="EMS", variable=price_post.mo_type, value=2).pack()
+        #price_post.mo_radio3 = Radiobutton(price_post.money, text="Online", variable=price_post.mo_type, value=3).pack()
+        price_post.mw_e_button = Button(price_post.money, text="Enter", command=price_post.c_money).pack()
+        price_post.mw_r_button = Button(price_post.money, text="Reset", command=price_post.money_res).pack()
+        price_post.money.mainloop()
+    def c_money(price_post):
+        """Call money class and return value"""
+        mo_calc = Money(price_post.mo_total.get(), price_post.mo_type.get())
         res_call = mo_calc.money()
-        main_win.paw_label2 = Label(main_win.money, text=res_call).pack()
-    def money_res(main_win):
-        main_win.money.destroy()
-        main_win.money_win()
-    def about_win(main_win):
-        """ about window """
-        main_win.w_about = Tk()
-        main_win.w_about.title("About")
-        main_win.aw_label1 = Label(main_win.w_about, text="PricePost, IT KMITL PSIT project").pack()
-        main_win.aw_label2 = Label(main_win.w_about, text="Semaster 1, 2014").pack()
-        main_win.aw_label3 = Label(main_win.w_about).pack()
-        main_win.aw_label4 = Label(main_win.w_about).pack()
-        main_win.aw_label5 = Label(main_win.w_about).pack()
-        main_win.aw_close = Button(main_win.w_about, text="Close", command=main_win.win_close).pack()
-        main_win.w_about.mainloop()
-    def win_close(main_win):
-        main_win.w_about.destroy()
+        price_post.mw_label2 = Label(price_post.money, text=res_call).pack()
+    def money_res(price_post):
+        """Reset money window"""
+        price_post.money.destroy()
+        price_post.money_win()
+    def about_win(price_post):
+        """About window"""
+        price_post.w_about = Tk()
+        price_post.w_about.title("About")
+        price_post.aw_label1 = Label(price_post.w_about, text="PricePost, IT KMITL PSIT project").pack()
+        price_post.aw_label2 = Label(price_post.w_about, text="Version 0.01 alpha, Developed by").pack()
+        price_post.aw_label3 = Label(price_post.w_about, text="Palit Wiboonlit, 57070085").pack()
+        price_post.aw_label4 = Label(price_post.w_about, text="57").pack()
+        price_post.aw_label5 = Label(price_post.w_about, text="Semaster 1, 2014").pack()
+        price_post.aw_close = Button(price_post.w_about, text="Close", command=price_post.aw_close).pack()
+        price_post.w_about.mainloop()
+    def aw_close(price_post):
+        """Close about window"""
+        price_post.w_about.destroy()
 class Postal():
-    def __init__(main_win, p_weight, p_type):
-        main_win.po_weight = int(p_weight)
-        main_win.po_type = int(p_type)
-    def parcel(main_win, fees=0):
-        return main_win.po_weight
+    def __init__(price_post, p_weight, p_type):
+        """Calling postal weight and type value"""
+        price_post.po_weight = int(p_weight)
+        price_post.po_type = int(p_type)
+    def postal(price_post, fees=0):
+        """Calculation"""
+        return price_post.po_weight
 class Parcel():
-    def __init__(main_win, weight):
-        main_win.pa_weight = int(weight)
-    def parcel(main_win):
-        if main_win.pa_weight> 20:
-            return "Limitation of sending is 20kg"
-        elif main_win.pa_weight<= 0:
-            return "minimum is 0.1 kg"
-        elif main_win.pa_weight > 0 or main_win.pa_weight <= 20:
+    def __init__(price_post, weight):
+        """Calling parcel weight value"""
+        price_post.pa_weight = int(weight)
+    def parcel(price_post):
+        """Calculation"""
+        if price_post.pa_weight > 20:
+            return "Limitation of sending is 20 kg"
+        elif price_post.pa_weight > 0 or price_post.pa_weight <= 20:
             fees = 20
-            for i in range(1, main_win.pa_weight):
+            for i in range(1, price_post.pa_weight):
                 fees += 15
             return "Total fees is : " + str(fees) + " BHT"
         else:
             return "Error, try again"
-class Money:
-    def __init__(main_win, m_total, m_type):
-        main_win.mo_total = int(m_total)
-        main_win.mo_type = int(m_type)
-    def money(main_win, fees=0):
-        #return main_win.mo_type
-        if main_win.mo_total > 50000:
-            return "Limitation of sending is 50,000BHT"
-        else:
-            #if main_win.mo_type == 3:
-                #fees = 50; main_win.mo_total = int(main_win.mo_total / 10000)
-                #for i in range(1, main_win.mo_total+1):
+class Money():
+    def __init__(price_post, m_total, m_type):
+        """Calling money to send and type value"""
+        price_post.mo_total = int(m_total)
+        price_post.mo_type = m_type
+    def money(price_post, fees=0):
+        """Calculation"""
+        if price_post.mo_total > 50000:
+            return "Limitation of sending is 50,000 BHT"
+        elif price_post.mo_total >= 1 and price_post.mo_total <= 50000:
+            return price_post.mo_total
+            #if price_post.mo_radio == 3:
+                #fees = 50; price_post.mo_total = int(price_post.mo_total / 10000)
+                #for i in range(1, price_post.mo_total+1):
                     #fees += 10
-            #elif main_win.mo_type == 1 or main_win.mo_type == 2:
-                #if main_win.mo_type == 1:
-            fees = 10.7
-                #elif main_win.mo_type == 2:
+            #elif price_post.mo_radio == 1 or price_post.mo_radio == 2:
+                #if price_post.mo_radio == 1:
+                    #fees = 10.7
+                #elif price_post.mo_radio == 2:
                     #fees = 44.94
-            main_win.mo_total = int(main_win.mo_total / 1000)
-            for i in range(1, main_win.mo_total+1):
-                fees += 2.14
-            return "Total fees is : " + "%.02f" % fees + " BHT"
+                #price_post.mo_total = int(price_post.mo_total / 1000)
+            #for i in range(1, price_post.mo_total+1):
+                #fees += 2.14
+            #return "Total fees is : " + "%.02f" % fees + " BHT"
+        else:
+            return "Error, try again"
 PricePost()
